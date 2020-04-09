d279df8a5da4ee03d34a4c6c5f44040333d7aab0
Pedro Ligabue <pliga@globo.com>
2020-04-08 21:23:54 -0300

    Criar Componente Para a Barra Lateral

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Box from '@material-ui/core/Box';
+import { List, ListItem, ListItemText, Typography, Divider, ListItemIcon, Icon } from '@material-ui/core';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import AssignmentOutlinedIcon from '@material-ui/icons/AssignmentOutlined';
+import CommentOutlinedIcon from '@material-ui/icons/CommentOutlined';
+import { Link } from 'react-router-dom';
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <Box>
+        <Link to="/" style={{ textDecoration: 'none', color: "inherit" }}>
+          <Typography variant="h5" noWrap color="textPrimary" style={{ padding: "2rem" }}>Automação</Typography>
+        </Link>
+        <List color="textPrimary">
+          <Link to="/dashboards" style={{ textDecoration: 'none', color: "inherit" }}>
+            <ListItem button>
+              <ListItemIcon><AccountCircleOutlinedIcon color="secondary" /></ListItemIcon>
+              <ListItemText primary={<Typography color="textPrimary">Dashboards</Typography>} />
+            </ListItem>
+          </Link>
+          <Divider />
+          <Link to="/algo" style={{ textDecoration: 'none', color: "inherit" }}>
+            <ListItem button>
+              <ListItemIcon><AssignmentOutlinedIcon color="secondary" /></ListItemIcon>
+              <ListItemText primary={<Typography color="textPrimary">Algo</Typography>} />
+            </ListItem>
+          </Link>
+          <Divider />
+          <Link to="/outracoisa" style={{ textDecoration: 'none', color: "inherit" }}>
+            <ListItem button>
+              <ListItemIcon><CommentOutlinedIcon color="secondary" /></ListItemIcon>
+              <ListItemText primary={<Typography color="textPrimary">Outra Coisa</Typography>} />
+            </ListItem>
+          </Link>
+        </List>
+      </Box>
+    );
+  }
+}
